4d1105c7f686ea0992b6939b8a97e31e24c3c953
Elijah Miernicki <miernickielijah@gmail.com>
2021-07-22 19:36:56 -0500

    user routes completed

---
+++ controllers/api/gameRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Games } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newGame = await Games.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newGame);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const gamesData = await Games.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!gamesData) {
+      res.status(404).json({ message: 'Turn back, you have made a grave mistake!' });
+      return;
+    }
+
+    res.status(200).json(gamesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

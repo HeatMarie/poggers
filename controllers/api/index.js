3f625b94fe3234cb68a39bfb50a870084d6ce08d
Elijah Miernicki <miernickielijah@gmail.com>
2021-07-22 19:42:37 -0500

    added tasks routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const profileRoutes = require('./profileRoutes');
 const gameRoutes = require('./gameRoutes');
 const tasksRoutes = require('./tasksRoutes');
 
-router.use('/users', userRoutes);
+router.use('/profile', profileRoutes);
 router.use('/games', gameRoutes);
 router.use('/tasks', tasksRoutes);
 
